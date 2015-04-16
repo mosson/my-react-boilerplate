24301d4cbf476899d1a5c7f8a3de07a81f74bdea
mosson <cucation@gmail.com>
2015-04-16 16:28:00 +0900

    superagentのモックサンプルを追加

---
+++ __tests__/superagent-spec.js
@@ -0,0 +1,42 @@
+var request = require('superagent');
+require('superagent-mock')(
+  request,
+  [
+    {
+      pattern: '/success',
+      fixtures: function () {
+        return 'success!';
+      },
+      callback: function (match, data) {
+        return {
+          body: data
+        };
+      }
+    },
+    {
+      pattern: '/error',
+      fixtures: function () {
+        return 'Error!';
+      },
+      callback: function (match, data) {
+        return {
+          body: data
+        };
+      }
+    }
+  ]
+);
+
+describe('superagentのテストの仕方', function () {
+  it("リクエストがモックされているかどうか", function(){
+    request.get("/success").end(function(err, res){
+      console.log(err, res);
+      expect(res).toEqual({body: 'success!'});
+    });
+
+    request.get("/error").end(function(err, res){
+      console.log(err, res);
+      expect(res).toEqual({body: 'Error!'});
+    });
+  });
+});
